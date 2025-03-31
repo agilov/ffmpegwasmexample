bb64729fc5bdcdb7f39abdefa93578fc9efb7e5d
agilov <agilovr@gmail.com>
2025-03-31 12:17:47 -0300

    added headers

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,11 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
   }
-  // server: {
-  //   headers: {
-  //     'Cross-Origin-Opener-Policy': 'same-origin',
-  //     'Cross-Origin-Embedder-Policy': 'require-corp'
-  //   }
-  // }
 })
